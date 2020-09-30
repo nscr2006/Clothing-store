bebbe5007a141974ca036eff78629b50adc23f94
Sharath Chandra Reddy Nimma <sharathchandrareddy@Sharaths-MBP.fios-router.home>
2020-09-29 20:24:47 -0400

    updating our app with shop page,preview collection preview content  and collection items

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import {Switch,Route} from 'react-router-dom';
 import './App.css';
+import ShopPage from './pages/shop/shop.component.jsx'
 import Homepage from './pages/homepage/homepage.component';
 
 
-const HatsPage=()=>(
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+
 function App() {
   return (
     <div>
       <Switch>
       <Route exact path='/' component={Homepage}/>
+      <Route exact path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
---
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import CollectionItem from '../collection-items/collection-items.component.jsx'
+import './preview-collection.styles.scss';
+
+const PreviewCollection=({title,items})=>(
+    <div className='preview-collection'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <div className='preview'>
+            {
+                items
+                .filter((item,idx)=>idx<4)
+                .map(({id,...otherItemProps})=>(
+                    <CollectionItem key={id}{ ...otherItemProps}/>
+                ))
+            }
+
+        </div>
+    </div>
+)
+ export default PreviewCollection;

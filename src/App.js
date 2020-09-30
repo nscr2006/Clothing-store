d02af55853067ffaa9cbd22249d0abac8ab81d22
Sharath Chandra Reddy Nimma <sharathchandrareddy@Sharaths-MBP.fios-router.home>
2020-09-29 20:46:57 -0400

    adding header components and styling them

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import {Switch,Route} from 'react-router-dom';
 import './App.css';
 import ShopPage from './pages/shop/shop.component.jsx'
 import Homepage from './pages/homepage/homepage.component';
+import Header from './components/header/header.component.jsx';
 
 
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
       <Route exact path='/' component={Homepage}/>
       <Route exact path='/shop' component={ShopPage}/>

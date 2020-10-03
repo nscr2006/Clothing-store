e7f514658954ed325ad7a55343a4777a7ebbd0a7
Sharath Chandra Reddy Nimma <sharathchandrareddy@Sharaths-MBP.fios-router.home>
2020-10-02 22:25:03 -0400

    Added cartIcon and cartDropdown components to our app,also implemented cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom';
 import {connect} from 'react-redux';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 import {auth} from '../../firebase/firebase.utils';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import CartIcon from '../cart-icon/cart-icon.component'
 import './header.styles.scss';
 
-const Header =(currentUser)=>(
+const Header =({currentUser,hidden})=>(
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo'/>
@@ -19,17 +21,21 @@ const Header =(currentUser)=>(
                 Contact
             </Link>
             {
-                currentUser?
+                currentUser?(
                 <div className='option' onClick={()=> auth.signOut()}>Sign Out</div>
-                :
+                ):(
                 <Link className='option' to='/signin'>Sign In</Link>
-            }
+                )}
+                <CartIcon/>
         </div>
+        {hidden ? null : <CartDropdown/>}
+        
     </div>
 )
 
-const mapStateProps=state=>({
-    currentUser:state.user.currentUser
+const mapStateProps=({user:{currentUser},cart:{hidden}})=>({
+    currentUser,
+    hidden
 })
 
  export default connect(mapStateProps)(Header);

8878a572612828bf45827fbbfc97ce650af805c3
Sharath Chandra Reddy Nimma <sharathchandrareddy@Sharaths-MBP.fios-router.home>
2020-10-04 18:04:15 -0400

    added addItem and removeItem functionality to checkout item component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,7 +4,7 @@ import {connect} from 'react-redux';
 import {toggleCartHidden} from '../../redux/cart/cart.actions';
 import {selectCartItemsCount} from '../../redux/cart/cart.selectors';
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
-// import CartActionTypes from '../../redux/cart/cart.types';
+
 import { createStructuredSelector } from 'reselect';
 
 

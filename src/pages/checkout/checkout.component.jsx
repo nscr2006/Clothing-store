992203b363196bd303ed20a0d8ed4925d7e83a63
Sharath Chandra Reddy Nimma <sharathchandrareddy@Sharaths-MBP.fios-router.home>
2020-10-05 21:03:22 -0400

    updated stripe integration and title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 import { createStructuredSelector} from 'reselect';
 import {selectCartItems,selectCartTotal} from '../../redux/cart/cart.selectors';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 
  
@@ -34,7 +35,7 @@ const CheckoutPage=({cartItems,total})=>(
         <div className='total'>
            TOTAL:${total}
         </div>
-
+        <StripeCheckoutButton price={total}/>
     </div>
 )
 

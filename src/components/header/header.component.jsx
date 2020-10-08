efc168068ce56c60fbd0afcb1251b675c45d12dc
Sharath Chandra Reddy Nimma <sharathchandrareddy@Sharaths-MBP.fios-router.home>
2020-10-07 22:32:50 -0400

    styled CSS in JS for some components and fixed the shop page view

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
 
@@ -7,41 +7,41 @@ import {createStructuredSelector} from 'reselect';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 
 
-import {auth} from '../../firebase/firebase.utils';
+import {auth} from '../../firebase/firebase.utils'
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import CartIcon from '../cart-icon/cart-icon.component'
 import {selectCartHidden} from '../../redux/cart/cart.selectors';
 import {selectCurrentUser} from '../../redux/user/user.selectors'
 
 
-import './header.styles.scss';
+import { HeaderContainer,LogoContainer,OptionDiv,OptionLink,OptionsContainer } from "./header.styles";
 
 
 
 const Header =({currentUser,hidden})=>(
-    <div className='header'>
-        <Link className='logo-container' to='/'>
+    <HeaderContainer>
+        <LogoContainer to='/'>
             <Logo className='logo'/>
 
-        </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'>
+        </LogoContainer>
+        <OptionsContainer>
+            <OptionLink to='/shop'>
                 SHOP
-            </Link>
-            <Link className='option' to='/shop'>
+            </OptionLink>
+            <OptionLink to='/shop'>
                 Contact
-            </Link>
+            </OptionLink>
             {
                 currentUser?(
-                <div className='option' onClick={()=> auth.signOut()}>SIGN OUT</div>
+                <OptionDiv onClick={()=> auth.signOut()}>SIGN OUT</OptionDiv>
                 ):(
-                <Link className='option' to='/signin'>Sign In</Link>
+                <OptionLink to='/signin'>Sign In</OptionLink>
                 )}
                 <CartIcon/>
-        </div>
+        </OptionsContainer>
         {hidden ? null : <CartDropdown/>}
         
-    </div>
+   </HeaderContainer>
 )
 
 const mapStateToProps=createStructuredSelector({
